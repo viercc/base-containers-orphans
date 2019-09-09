2f88d5b8f114b0fe73381a4659fea1dc4f70044d
Koji Miyazato <viercc@gmail.com>
2019-09-10 08:12:15 +0900

    Test against ghc-8.8 containers-(0.6.1.1|0.6.2.2)

--- build_matrix.sh
+++ build_matrix.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-containers_versions='0.5.7.1 0.5.8.2 0.5.9.2 0.5.10.1 0.5.11.0 0.6.0.1'
+set -e
 
 function build() {
-    cabal new-build --enable-tests -w ghc-$1 --constraint="containers ==$2" \
-      -O0
+    echo "build GHC=$1 containers=$2"
+    cabal v2-build -v0 -O0 --enable-tests \
+          -w ghc-$1 --constraint="containers ==$2" \
+          && echo "Ok"
 }
 
 build 8.0.2 0.5.7.1
@@ -13,6 +15,8 @@ build 8.0.2 0.5.9.2
 build 8.0.2 0.5.10.1
 build 8.0.2 0.5.11.0
 build 8.0.2 0.6.0.1
+build 8.0.2 0.6.1.1
+build 8.0.2 0.6.2.1
 
 build 8.2.2 0.5.7.1
 build 8.2.2 0.5.8.2
@@ -20,9 +24,20 @@ build 8.2.2 0.5.9.2
 build 8.2.2 0.5.10.1
 build 8.2.2 0.5.11.0
 build 8.2.2 0.6.0.1
+build 8.2.2 0.6.1.1
+build 8.2.2 0.6.2.1
 
-build 8.4.3 0.5.11.0
-build 8.4.3 0.6.0.1
+build 8.4.4 0.5.11.0
+build 8.4.4 0.6.0.1
+build 8.4.4 0.6.1.1
+build 8.4.4 0.6.2.1
 
-build 8.6.3 0.5.11.0
-build 8.6.3 0.6.0.1
+build 8.6.5 0.5.11.0
+build 8.6.5 0.6.0.1
+build 8.6.5 0.6.1.1
+build 8.6.5 0.6.2.1
+
+build 8.8.1 0.5.11.0
+build 8.8.1 0.6.0.1
+build 8.8.1 0.6.1.1
+build 8.8.1 0.6.2.1
